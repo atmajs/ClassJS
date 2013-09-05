d5a88e7d79c9058a147aa10059f532c87954a155
Alexander Kit <source.kit@gmail.com>
2013-09-05 20:45:20 +0200

    [fix] raw commonjs module

--- builds/class.js
+++ builds/class.js
@@ -1,22 +1,33 @@
 (function(root, factory){
 	"use strict";
 
-	var _global, _exports;
+	var _isCommonJS = false,
+		_global,
+		_exports;
 	
-	if (typeof exports !== 'undefined' && (root === exports || root == null)){
+	if (typeof exports !== 'undefined' && (root == null || root === exports || root === global)){
 		// raw nodejs module
-    	_global = _exports = global;
+        _global = global;
+		_isCommonJS = true;
     }
 	
 	if (_global == null) {
-		_global = typeof window === 'undefined' ? global : window;
+		_global = typeof window === 'undefined'
+			? global
+			: window
+			;
 	}
+	
 	if (_exports == null) {
 		_exports = root || _global;
 	}
 	
 	
 	factory(_global, _exports);
+	
+	if (_isCommonJS) {
+		module.exports = _exports.Class;
+	}
 	
 }(this, function(global, exports){
 	"use strict";
