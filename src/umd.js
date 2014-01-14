c6cccaff3f40cb9ff13224bac4403722333b2b8e
Alexander Kit <source.kit@gmail.com>
2014-01-14 19:14:47 +0100

    [++] use UMD same as in MaskJS

--- src/umd.js
+++ src/umd.js
@@ -1,33 +1,33 @@
 (function(root, factory){
 	"use strict";
 
-	var _isCommonJS = false,
-		_global,
-		_exports;
-	
-	if (typeof exports !== 'undefined' && (root == null || root === exports || root === global)){
-		// raw nodejs module
-        _global = global;
-		_isCommonJS = true;
-    }
-	
-	if (_global == null) {
-		_global = typeof window === 'undefined'
+	var _global = typeof window === 'undefined' || window.navigator == null
 			? global
 			: window
-			;
-	}
+			,
+		_exports;
+
+    
+	if (typeof exports !== 'undefined' && (root == null || root === exports || root === _global)){
+		// raw commonjs module
+        root = exports;
+    }
 	
-	if (_exports == null) {
-		_exports = root || _global;
-	}
-	
-	
-	factory(_global, _exports);
-	
-	if (_isCommonJS) {
-		module.exports = _exports.Class;
-	}
+    _exports = root || _global;
+    
+
+    function construct(){
+
+        return factory(_global, _exports);
+    };
+
+    
+    if (typeof define === 'function' && define.amd) {
+        return define(construct);
+    }
+    
+	// Browser OR Node
+    return construct();
 	
 }(this, function(global, exports){
 	"use strict";
