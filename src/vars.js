9a5e70646def2f06a8bdc2fe7f2640707ce664dd
Alex Kit <source.kit@gmail.com>
2014-08-15 16:57:01 +0200

    v1.0.68

--- src/vars.js
+++
@@ -1,9 +0,0 @@
-var _Array_slice = Array.prototype.slice,
-	_Array_sort = Array.prototype.sort,
-	
-	_cfg = {
-		ModelHost: null, // @default: Class.Model
-	};
-	
-
-var str_CLASS_IDENTITY = '__$class__';
